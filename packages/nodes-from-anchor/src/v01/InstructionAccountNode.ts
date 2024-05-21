2c3bdf312d018ca27e3a1657b1240d053e25f4dd
Kyle Espinola <kyle.s.espinola@gmail.com>
2024-05-21 13:27:25 +0200

    feat: express anchor account seed definitions in instructions as defaultValue on the instruction account

--- packages/nodes-from-anchor/src/v01/InstructionAccountNode.ts
+++ packages/nodes-from-anchor/src/v01/InstructionAccountNode.ts
@@ -1,6 +1,20 @@
-import { InstructionAccountNode, instructionAccountNode, publicKeyValueNode } from '@kinobi-so/nodes';
-
-import { IdlV01InstructionAccount, IdlV01InstructionAccountItem } from './idl';
+import {
+    accountValueNode,
+    InstructionAccountNode,
+    instructionAccountNode,
+    pdaSeedValueNode,
+    pdaValueNode,
+    publicKeyValueNode,
+    pdaNode,
+    PdaSeedNode,
+    variablePdaSeedNode,
+    publicKeyTypeNode,
+    constantPdaSeedNodeFromBytes,
+    hex,
+    PdaSeedValueNode,
+} from '@kinobi-so/nodes';
+import { KinobiError, KINOBI_ERROR__ANCHOR__SEED_KIND_UNIMPLEMENTED } from '@kinobi-so/errors';
+import { IdlV01InstructionAccount, IdlV01Seed, IdlV01InstructionAccountItem } from './idl';
 
 export function instructionAccountNodesFromAnchorV01(idl: IdlV01InstructionAccountItem[]): InstructionAccountNode[] {
     return idl.flatMap(account =>
@@ -20,6 +34,40 @@ export function instructionAccountNodeFromAnchorV01(idl: IdlV01InstructionAccoun
 
     if (idl.address) {
         defaultValue = publicKeyValueNode(idl.address, name);
+    }
+
+    if (idl.pda) {
+        const [seeds, lookups] = idl.pda.seeds.reduce(
+            ([seeds, lookups], seed: IdlV01Seed) => {
+                const kind = seed.kind;
+
+                switch (kind) {
+                    case 'const':
+                        return [
+                            [...seeds, constantPdaSeedNodeFromBytes('base16', hex(new Uint8Array(seed.value)))],
+                            lookups,
+                        ];
+                    case 'account':
+                        return [
+                            [...seeds, variablePdaSeedNode(seed.path, publicKeyTypeNode())],
+                            [...lookups, pdaSeedValueNode(seed.path, accountValueNode(seed.path))],
+                        ];
+                    default:
+                        throw new KinobiError(KINOBI_ERROR__ANCHOR__SEED_KIND_UNIMPLEMENTED, {
+                            kind,
+                        });
+                }
+            },
+            <[PdaSeedNode[], PdaSeedValueNode[]]>[[], []],
+        );
+
+        defaultValue = pdaValueNode(
+            pdaNode({
+                name,
+                seeds,
+            }),
+            lookups,
+        );
     }
 
     return instructionAccountNode({
